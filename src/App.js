263d1790c3ea910b943ae54f211924695df0969f
YourMCGeek <yourmcgeek@icloud.com>
2017-08-22 11:12:09 -0400

    Commit files for 3rd Party Node Server

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { 
+  Component 
+} from 'react';
+//import logo from './logo.svg';
+//import './App.css';
+
+/*var googleMapsClient = require('@google/maps').createClient({
+  key: 'AIzaSyDuw39SiC8G8-8RPZhXRSkFSQuDn9PPa_g'
+});*/
+
+class App extends Component {
+  render() {
+    return (
+      <div className="map">
+      </div>
+    );
+  }
+}
+
+export default App;
